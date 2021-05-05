b60f47e69634f91cf153c1e5d5840db2107aaad4
Michael Pilosov <consistentbayes@gmail.com>
2021-05-05 21:33:58 +0000

    cmdline + streamlit functionality

--- colorize/app.py
+++ colorize/app.py
@@ -13,16 +13,22 @@ def maybe_convert_to_grayscale(image: Image) -> np.ndarray:
     If image is already single-channel, does nothing.
     """
     pixel = image.getpixel((1,1))
-    num_channels = len(pixel)
+    if isinstance(pixel, (int, float)):
+        num_channels = 1
+    else:
+        num_channels = len(pixel)
+
     if num_channels == 1:
         return image
+
     return ImageOps.grayscale(ImageOps.grayscale(image))
 
 
 # @st.cache
-def load_image_from_url(url: str) -> ByteString:
+def load_image_from_url(url: str) -> Image:
     content = requests.get(url).content
-    return io.BytesIO(content)
+    bytes_data = io.BytesIO(content)
+    return bytes_to_image(bytes_data)
 
 
 def load_image_from_upload():
@@ -31,44 +37,47 @@ def load_image_from_upload():
 
 def colorize_with_mpl(image: Image, colormap: str = 'hot') -> np.ndarray:
     data = np.array(list(image.getdata())).reshape(image.size[::-1])
-    # plt.savefig('test.png')
-    fig, ax = plt.subplots()
-    ax.imshow(data, cmap=colormap)
-    ax.set(frame_on=False)
-    ax.axis('off')
-    st.pyplot(fig)
-    return data
+    fig = save_image(data, colormap, 'test.png')
+    print('saved')
+    return fig
+
+
+def save_image(data, cm, filename=None):
+    sizes = np.shape(data)
+    height = float(sizes[0])
+    width = float(sizes[1])
+     
+    fig = plt.figure()
+    fig.set_size_inches(width/height, 1, forward=False)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+ 
+    ax.imshow(data, cmap=cm)
+    if filename:
+        fig.savefig(filename, dpi=height) 
+    plt.close()
+    return fig
 
 def colorize_with_nn():
     pass
 
+
 def colorize_with_pil(image: Image) -> Image:
     pass
 
-def main(bytes_data: ByteString, colormap='viridis'):
-    image = Image.open(bytes_data, 'r')
-    image = maybe_convert_to_grayscale(image)
-    data = colorize_with_mpl(image, colormap)
-    return data
 
-if __name__ == '__main__':
-    # get url from user in cmd line
-    import sys
-    if len(sys.argv) > 1:
-        DATA_URL = sys.argv[1]
-    else:
-        DATA_URL = 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-gold-select-201810?wid=1078&hei=624&fmt=jpeg&qlt=80&.v=1603332211000'
-
-    ##### START OF APP #####
+def header():
     st.markdown('# Colorize Photos')
     st.markdown('Upload or link to a black and white photo (or color...) and have it be colorized')
     st.markdown('TODO:')
     st.markdown('- add more ways of colorizing')
     st.markdown('- add more colormap options')
     st.markdown('***')
-    all_colormaps = ['viridis', 'hot']  # FIXME get full list
-    bytes_data = None
 
+
+def get_image_with_st() -> Image:
+    bytes_data = None
     input_mode = st.selectbox(label='Image Input Mode', options=['url', 'file'])
     if input_mode == 'url':
         data_url = st.text_input("URL of Image (Right Click > Copy Image Location)",
@@ -82,10 +91,48 @@ if __name__ == '__main__':
         if uploaded_file is not None:
             bytes_data = uploaded_file
             st.image(bytes_data)
+    
+    image = bytes_to_image(bytes_data)
+    return image
 
-    if bytes_data:
+
+def bytes_to_image(bytes_data: ByteString) -> Image:
+    if not bytes_data:
+        return None
+    image = Image.open(bytes_data, 'r')
+    image = maybe_convert_to_grayscale(image)
+    return image
+
+
+def colorize_with_st(image: Image):
+    if not image:
+        return None
+    method = st.selectbox(label='Select Method', options=['mpl'])
+    all_colormaps = ['viridis', 'hot']  # FIXME get full list
+    if method == 'mpl':
         colormap = st.selectbox(label='Select Color Map', options=all_colormaps)
-        main(bytes_data, colormap) 
-    ######### END ##########
+        fig = colorize_with_mpl(image, colormap)
+    else:
+        fig = None
+    
+    st.pyplot(fig)
 
 
+if __name__ == '__main__':
+    # get url from user in cmd line
+    import sys
+    nargs = len(sys.argv)
+    print("Testing syntax: `python app.py URL`")
+    if nargs == 1:
+        streamlit = True
+    else:
+        DATA_URL = sys.argv[1]
+        streamlit = False
+
+    if streamlit:
+        header()
+        image = get_image_with_st()
+        colorize_with_st(image)
+    else:
+        image = load_image_from_url(DATA_URL)
+        colorize_with_mpl(image, colormap='viridis')
--- colorize/run.sh
+++ colorize/run.sh
@@ -3,4 +3,4 @@ streamlit run \
     --browser.serverAddress 0.0.0.0 \
     --server.enableCORS False \
     --server.enableXsrfProtection False \
-    $@
+    app.py
